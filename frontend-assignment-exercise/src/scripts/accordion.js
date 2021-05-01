d1c0ea1df4cc1f437b9e10e3b016c8b0ded6e3c5
Chris Withall <chriswithall@Chriss-MacBook-Pro.local>
2021-05-01 19:05:01 +0200

    Added focusing to the accordion

--- frontend-assignment-exercise/src/scripts/accordion.js
+++ frontend-assignment-exercise/src/scripts/accordion.js
@@ -1,11 +1,19 @@
 const accordionToggle = (id) => {
+  let selectedQuestion = document.getElementById(`accordion${id}`);
   let selectedPanel = document.getElementsByClassName(
-    `faq__container--${id}`
+    `faq__container--panel${id}`
   )[0];
-  console.log('SELECTED PANEL ', selectedPanel, ' ID ', id);
   if (selectedPanel.className.includes('show')) {
     selectedPanel.className = selectedPanel.className.replace('show', '');
   } else {
     selectedPanel.className += ' show';
+  }
+  if (selectedQuestion.className.includes('focused')) {
+    selectedQuestion.className = selectedQuestion.className.replace(
+      'focused',
+      ''
+    );
+  } else {
+    selectedQuestion.className += ' focused';
   }
 };
