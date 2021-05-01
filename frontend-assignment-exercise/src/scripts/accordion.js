1067d40788754875c8f5b3667751593bc8636570
Chris Withall <chriswithall@Chriss-MacBook-Pro.local>
2021-05-01 19:17:20 +0200

    Added notes on logic

--- frontend-assignment-exercise/src/scripts/accordion.js
+++ frontend-assignment-exercise/src/scripts/accordion.js
@@ -1,8 +1,19 @@
 const accordionToggle = (id) => {
+  // Get all questions
+  // Remove all focused from classNames that are not the current
+  // Get current question
+  // Add focused using id
+
+  // Get all panels
+  // Remove all show from classNames that are not the current
+  // Get current panel
+  // Add show to panel unless it already
+
   let selectedQuestion = document.getElementById(`accordion${id}`);
   let selectedPanel = document.getElementsByClassName(
     `faq__container--panel${id}`
   )[0];
+
   if (selectedPanel.className.includes('show')) {
     selectedPanel.className = selectedPanel.className.replace('show', '');
   } else {
